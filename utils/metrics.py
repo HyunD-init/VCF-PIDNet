33b5561edfbfb55ff6a936a58063b747dbf28e48
spinai_dev <>
2025-06-12 20:37:57 +0900

    [fix] metric 수정

--- utils/metrics.py
+++ utils/metrics.py
@@ -15,19 +15,8 @@ def meanIoU(y_pred, y_true):
 
     C = y_true.size()[-3]
 
-    S1 = y_pred[:,[a for a in range(C-1)],:,:]
-    S1 = torch.argmax(S1, dim=-3)
-    S1 = F.one_hot(S1, num_classes=C-1).permute(0, 3, 1, 2).to(torch.int32)
-
-    latSacrum = y_pred[:,[a for a in range(C-2)] + [C-1],:,:]
-    latSacrum = torch.argmax(latSacrum, dim=-3)
-    latSacrum = F.one_hot(latSacrum, num_classes=C-1).permute(0, 3, 1, 2).to(torch.int32)
-    
-    y_pred = torch.concat([S1[:,:-1,:,:]&latSacrum[:,:-1,:,:], S1[:,-1,:,:].unsqueeze(1), latSacrum[:,-1,:,:].unsqueeze(1)],dim=-3)
-
-
-    # 자 이후 처리를 생각해보자 | or & ,etc.
-    y_pred = y_pred.to(torch.int32)
+    arg_pred = torch.argmax(y_pred, dim=-3)
+    y_pred = F.one_hot(arg_pred, num_classes=C).permute(0, 3, 1, 2).to(torch.int32)
     y_true = y_true.to(torch.int32)
 
     inter = y_true & y_pred
@@ -40,18 +29,9 @@ def meanIoU(y_pred, y_true):
 def f1_score(y_pred, y_true):
 
     C = y_true.size()[-3]
-    S1 = y_pred[:,[a for a in range(C-1)],:,:]
-    S1 = torch.argmax(S1, dim=-3)
-    S1 = F.one_hot(S1, num_classes=C-1).permute(0, 3, 1, 2).to(torch.int32)
-
-    latSacrum = y_pred[:,[a for a in range(C-2)] + [C-1],:,:]
-    latSacrum = torch.argmax(latSacrum, dim=-3)
-    latSacrum = F.one_hot(latSacrum, num_classes=C-1).permute(0, 3, 1, 2).to(torch.int32)
-
-    y_pred = torch.concat([S1[:,:-1,:,:]&latSacrum[:,:-1,:,:], S1[:,-1,:,:].unsqueeze(1), latSacrum[:,-1,:,:].unsqueeze(1)],dim=-3)
-
-    # 자 이후 처리를 생각해보자 | or & ,etc.
-    y_pred = y_pred.to(torch.int32)
+    
+    arg_pred = torch.argmax(y_pred, dim=-3)
+    y_pred = F.one_hot(arg_pred, num_classes=C).permute(0, 3, 1, 2).to(torch.int32)
     y_true = y_true.to(torch.int32)
     
     TP_2 = (y_true & y_pred) * 2
